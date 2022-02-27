1ad4862a173467d196013a9262a0dc39614a0b9f
Sanjeev Thiyagarajan <sanjeevkt720@gmail.com>
2022-02-27 00:35:27 -0500

    part 40 update-delete-by-id

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Delete, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/auth/user.entity';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Controller('comments')
 export class CommentsController {
@@ -16,5 +26,26 @@ export class CommentsController {
     @GetUser() user: User,
   ) {
     return this.commentsService.createComment(createCommentDto, user);
+  }
+
+  @Delete('/:id')
+  @UseGuards(AuthGuard())
+  deleteComment(@Param('id') id: number, @GetUser() user: User) {
+    return this.commentsService.deleteComment(id, user);
+  }
+
+  @Patch('/:id')
+  @UseGuards(AuthGuard())
+  updateComment(
+    @Param('id') id: number,
+    @GetUser() user: User,
+    @Body() updateCommentDto: UpdateCommentDto,
+  ) {
+    return this.commentsService.updateComment(id, updateCommentDto, user);
+  }
+
+  @Get('/:id')
+  getCommentById(@Param('id') id: number) {
+    return this.commentsService.getCommentById(id);
   }
 }
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,9 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/auth/user.entity';
 import { ContentRepository } from 'src/content/content.repository';
 import { CommentsRepository } from './conmment.repository';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Injectable()
 export class CommentsService {
@@ -28,6 +34,59 @@ export class CommentsService {
     }
     const comment = this.commentsRepository.create({ text, user, content });
     await this.commentsRepository.save(comment);
+    return comment;
+  }
+
+  async deleteComment(id: number, user: User) {
+    const comment = await this.commentsRepository.findOne({
+      where: { id },
+      relations: ['user'],
+    });
+    console.log(comment);
+
+    if (!comment) {
+      throw new NotFoundException(`Comment with id: ${id} was not found`);
+    }
+
+    if (comment.user.id !== user.id) {
+      throw new ForbiddenException();
+    }
+
+    await this.commentsRepository.delete(id);
+    return;
+  }
+
+  async updateComment(
+    id: number,
+    updateCommentDto: UpdateCommentDto,
+    user: User,
+  ) {
+    const comment = await this.commentsRepository.findOne({
+      where: { id },
+      relations: ['user'],
+    });
+
+    if (!comment) {
+      throw new NotFoundException(`Comment with id: ${id} was not found`);
+    }
+
+    if (comment.user.id !== user.id) {
+      throw new ForbiddenException();
+    }
+
+    Object.assign(comment, updateCommentDto);
+    await this.commentsRepository.save(comment);
+    return comment;
+  }
+
+  async getCommentById(id) {
+    const comment = await this.commentsRepository.findOne({
+      where: { id },
+      relations: ['user'],
+    });
+    if (!comment) {
+      throw new NotFoundException(`Comment with id: ${id} was not found`);
+    }
     return comment;
   }
 }
